6ffa068374e6b284ac8245ec8d9c9e0c92ac8913
3174N <eitan.levy.159@gmail.com>
2022-05-21 18:19:51 +0000

    Update serialization

--- Trivia/Trivia/Requests.h
+++ Trivia/Trivia/Requests.h
@@ -1,8 +1,15 @@
-#pragma once
+#ifndef REQUESTS_H_
+#define REQUESTS_H_
 
 #include <iostream>
 
-enum requestCodes { RQ_LOGIN = 1, RQ_SIGNUP };
+enum requestCodes {
+    RQ_LOGIN = 1,
+    RQ_SIGNUP,
+    RQ_GETPLAYERS,
+    RQ_JOINROOM,
+    RQ_CREATEROOM
+};
 
 typedef struct LoginRequest {
     std::string username;
@@ -17,3 +24,20 @@ typedef struct SignupRequest {
     std::string phone;
     std::string date;
 } SignupRequest;
+
+typedef struct GetPlayersInRoomRequest {
+    unsigned int roomId;
+} GetPlayersInRoomRequest;
+
+typedef struct JoinRoomRequest {
+    unsigned int roomId;
+} JoinRoomRequest;
+
+typedef struct CreateRoomRequest {
+    std::string roomName;
+    unsigned int maxUsers;
+    unsigned int questionCount;
+    unsigned int answerTimeout;
+} CreateRoomRequest;
+
+#endif // !REQUESTS_H_
--- Trivia/Trivia/Responses.h
+++ Trivia/Trivia/Responses.h
@@ -1,8 +1,23 @@
-#pragma once
+#ifndef RESPONSES_H_
+#define RESPONSES_H_
 
+#include "IDatabase.h"
+#include "Room.h"
 #include <iostream>
+#include <vector>
 
-enum responseCodes { RS_ERROR = -1, RS_LOGIN = 1, RS_SIGNUP };
+enum responseCodes {
+    RS_ERROR = -1,
+    RS_LOGIN = 1,
+    RS_SIGNUP,
+    RS_LOGOUT,
+    RS_GETROOMS,
+    RS_PLAYERSINROOM,
+    RS_HIGHSCORE,
+    RS_PERSONALSTATS,
+    RS_JOINROOM,
+    RS_CREATEROOM
+};
 
 typedef struct LoginResponse {
     unsigned int status;
@@ -12,6 +27,39 @@ typedef struct SignupResponse {
     unsigned int status;
 } SignupResponse;
 
+typedef struct LogoutResponse {
+    unsigned int status;
+} LogoutResponse;
+
+typedef struct GetRoomsResponse {
+    unsigned int status;
+    std::vector<RoomData> rooms;
+} GetRoomsResponse;
+
+typedef struct GetPlayersInRoomResponse {
+    std::vector<std::string> players;
+} GetPlayersInRoomResponse;
+
+typedef struct GetHighScoreResponse {
+    unsigned int status;
+    std::vector<std::string> statistics;
+} GetHighScoreResponse;
+
+typedef struct GetPersonalStatsResponse {
+    unsigned int status;
+    Statistics statistics;
+} GetPersonalStatsResponse;
+
+typedef struct JoinRoomResponse {
+    unsigned int status;
+} JoinRoomResponse;
+
+typedef struct CreateRoomResponse {
+    unsigned int status;
+} CreateRoomResponse;
+
 typedef struct ErrorResponse {
     std::string message;
 } ErrorResponse;
+
+#endif // !RESPONSES_H_
