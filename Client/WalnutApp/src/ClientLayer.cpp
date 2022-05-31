07e4f9358313a0a6fbc0ea6a66b8984518bbb78d
3174N <eitan.levy.159@gmail.com>
2022-05-31 14:08:42 +0000

    Update client

--- Client/WalnutApp/src/ClientLayer.cpp
+++ Client/WalnutApp/src/ClientLayer.cpp
@@ -1,38 +1,63 @@
 #include "ClientLayer.h"
 
+void ClientLayer::OnAttach() {
+    Communicator::communicator.startNewConnection();
+}
+
 void ClientLayer::OnUIRender() {
     updateDeltaTime();
 
     ImGui::Begin("Trivia");
-    switch (m_screen) {
-    case Screens::Login:
-        renderLogin();
-        break;
-    case Screens::Signup:
-        renderSignup();
-        break;
-    case Screens::Menu:
-        renderMenu();
-        break;
-    case Screens::CreateRoom:
-        renderCreateRoom();
-        break;
-    case Screens::JoinRoom:
-        renderJoinRoom();
-        break;
-    case Screens::PersonalStats:
-        renderPersonalStats();
-        break;
-    case Screens::Highscore:
-        renderHighscore();
-        break;
-    default:
-        break;
+
+    static std::string errStr;
+    try {
+        switch (m_screen) {
+        case Screens::Login:
+            renderLogin();
+            break;
+        case Screens::Signup:
+            renderSignup();
+            break;
+        case Screens::Menu:
+            renderMenu();
+            break;
+        case Screens::CreateRoom:
+            renderCreateRoom();
+            break;
+        case Screens::JoinRoom:
+            renderJoinRoom();
+            break;
+        case Screens::PersonalStats:
+            renderPersonalStats();
+            break;
+        case Screens::Highscore:
+            renderHighscore();
+            break;
+        case Screens::Room:
+            renderRoom();
+            break;
+        }
+    } catch (const std::exception &e) {
+        errStr = e.what();
+        ImGui::OpenPopup("Error");
     }
+
+    if (ImGui::BeginPopupModal("Error", NULL,
+                               ImGuiWindowFlags_AlwaysAutoResize)) {
+        ImGui::Text(errStr.c_str());
+
+        if (ImGui::Button("Close")) {
+            ImGui::CloseCurrentPopup();
+        }
+        ImGui::EndPopup();
+    }
+    
     ImGui::End();
 
     ImGui::ShowDemoWindow();
 }
+
+void ClientLayer::OnDetach() {}
 
 /**
  * @brief Creates a tooltip help marker.
@@ -61,12 +86,37 @@ void ClientLayer::updateDeltaTime() {
     m_deltaTime = duration.count();
 
     // Limit framerate.
-    if (LOW_DELTA > m_deltaTime)
+    if (LOW_DELTA > m_deltaTime) {
+        // Sleep until next frame
+        std::chrono::duration<double, std::milli> delta_ms(LOW_DELTA * 1000 -
+                                                           m_deltaTime * 1000);
+        auto delta_ms_duration =
+            std::chrono::duration_cast<std::chrono::milliseconds>(delta_ms);
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds(delta_ms_duration.count()));
         m_deltaTime = LOW_DELTA;
+    }
     if (HIGH_DELTA < m_deltaTime)
         m_deltaTime = HIGH_DELTA;
 
     lastTime = currTime;
+}
+
+/**
+ * @brief Counts down.
+ *
+ * @param counter Counter variable.
+ * @return true If counter reached 0.
+ * @return false If not.
+ */
+bool ClientLayer::count(float *counter) {
+    if (*counter <= 0) {
+        *counter = m_maxCounterTime;
+        return true;
+    }
+
+    *counter -= m_deltaTime;
+    return false;
 }
 
 /**
@@ -80,17 +130,13 @@ void ClientLayer::renderLogin() {
     ImGui::InputTextWithHint("##password", "Password", password,
                              IM_ARRAYSIZE(password));
     ImGui::SameLine();
-    HelpMarker("has to contain at least 1 lowercase letter, 1 uppercase letter, "
+    HelpMarker(
+        "has to contain at least 1 lowercase letter, 1 uppercase letter, "
         "1 digit and 1 non-alphanumeric character");
 
     if (ImGui::Button("Login")) {
-        try {
-            if (RequestHandler::loginRequest(username, password).status ==
-                RS_LOGIN)
-                m_screen = Screens::Menu;
-        } catch (const std::exception &e) {
-            std::cout << e.what() << std::endl;
-        }
+        if (RequestHandler::loginRequest(username, password).status == RS_LOGIN)
+            m_screen = Screens::Menu;
     }
 
     if (ImGui::Button("Signup")) {
@@ -110,7 +156,8 @@ void ClientLayer::renderSignup() {
     ImGui::InputTextWithHint("##password", "Password", password,
                              IM_ARRAYSIZE(password));
     ImGui::SameLine();
-    HelpMarker("has to contain at least 1 lowercase letter, 1 uppercase letter, "
+    HelpMarker(
+        "has to contain at least 1 lowercase letter, 1 uppercase letter, "
         "1 digit and 1 non-alphanumeric character");
     static char email[128] = "";
     ImGui::InputTextWithHint("##email", "Email", email, IM_ARRAYSIZE(email));
@@ -126,17 +173,10 @@ void ClientLayer::renderSignup() {
     HelpMarker("either DD/MM/YYYY or DD.MM.YYYY");
 
     if (ImGui::Button("Signup")) {
-        try {
-            if (RequestHandler::signupRequest(username, password, email,
-                                              address, phone, birthDate)
-                    .status == RS_SIGNUP) {
-                if (RequestHandler::loginRequest(username, password).status ==
-                    RS_LOGIN)
-                    m_screen = Screens::Menu;
-            }
-        } catch (const std::exception &e) {
-            std::cout << e.what() << std::endl;
-        }
+        RequestHandler::signupRequest(username, password, email, address, phone,
+                                      birthDate);
+        RequestHandler::loginRequest(username, password);
+        m_screen = Screens::Menu;
     }
 
     if (ImGui::Button("Login")) {
@@ -156,7 +196,7 @@ void ClientLayer::renderMenu() {
     if (ImGui::Button("Statistics"))
         m_screen = Screens::PersonalStats;
     if (ImGui::Button("Exit")) {
-        // TODO
+        m_app->Close();
     }
 }
 
@@ -175,10 +215,12 @@ void ClientLayer::renderCreateRoom() {
     ImGui::InputInt("Answer timeout", &answerTimeout);
 
     if (ImGui::Button("Create room")) {
-        if (RequestHandler::createRoomRequest(roomName, maxUsers, questionCount,
-                                              answerTimeout)
-                .status == RS_CREATEROOM)
-            m_screen = Screens::Menu;
+        RequestHandler::createRoomRequest(roomName, maxUsers, questionCount,
+                                          answerTimeout);
+
+        m_isAdmin = true;
+
+        m_screen = Screens::Room;
     }
 
     if (ImGui::Button("Back"))
@@ -189,7 +231,11 @@ void ClientLayer::renderCreateRoom() {
  * @brief Renders joinRoom screen.
  */
 void ClientLayer::renderJoinRoom() {
-    GetRoomsResponse rooms = RequestHandler::getRoomsRequest();
+    GetRoomsResponse rooms;
+    static float getRoomsCounter = 0;
+    if (count(&getRoomsCounter))
+        rooms = RequestHandler::getRoomsRequest();
+
     static int selectedRoom = -1;
 
     if (ImGui::BeginTable("Rooms", 4,
@@ -210,8 +256,10 @@ void ClientLayer::renderJoinRoom() {
             if (r.isActive)
                 continue;
 
-            GetPlayersInRoomResponse players =
-                RequestHandler::getPlayersInRoomRequest(r.id);
+            GetPlayersInRoomResponse players;
+            static float getPlayersCounter = 0;
+            if (count(&getPlayersCounter))
+                players = RequestHandler::getPlayersInRoomRequest(r.id);
             std::string playersStr = std::to_string(players.players.size()) +
                                      "/" + std::to_string(r.maxPlayers);
 
@@ -233,7 +281,10 @@ void ClientLayer::renderJoinRoom() {
     if (selectedRoom != -1) {
         if (ImGui::Button("Join room")) {
             RequestHandler::joinRoomRequest(selectedRoom);
-            m_screen = Screens::Menu;
+
+            m_isAdmin = false;
+
+            m_screen = Screens::Room;
         }
     }
 
@@ -250,7 +301,8 @@ void ClientLayer::renderPersonalStats() {
         return;
     }
 
-    GetPersonalStatsResponse stats = RequestHandler::getPersonalStatsRequest();
+    static GetPersonalStatsResponse stats =
+        RequestHandler::getPersonalStatsRequest();
 
     std::string gamesWon = std::to_string(stats.statistics.gamesWon) + "/" +
                            std::to_string(stats.statistics.totalGames);
@@ -258,7 +310,7 @@ void ClientLayer::renderPersonalStats() {
         std::to_string(stats.statistics.correctAnswers) + "/" +
         std::to_string(stats.statistics.totalAnswers);
 
-    if (ImGui::BeginTable("Rooms", 2,
+    if (ImGui::BeginTable("Statistics", 2,
                           ImGuiTableFlags_Resizable |
                               ImGuiTableFlags_NoSavedSettings |
                               ImGuiTableFlags_Borders)) {
@@ -302,7 +354,8 @@ void ClientLayer::renderHighscore() {
         return;
     }
 
-    GetHighScoreResponse highscore = RequestHandler::getHighscoreRequest();
+    static GetHighScoreResponse highscore =
+        RequestHandler::getHighscoreRequest();
 
     for (auto i : highscore.statistics) {
         ImGui::Text(i.c_str());
@@ -310,4 +363,60 @@ void ClientLayer::renderHighscore() {
 
     if (ImGui::Button("Back"))
         m_screen = Screens::Menu;
+}
+
+/**
+ * @brief Render room screen.
+ */
+void ClientLayer::renderRoom() {
+    GetRoomStateResponse roomState;
+    static float getRoomSateCounter = 0;
+    if (count(&getRoomSateCounter))
+        roomState = RequestHandler::getRoomStateRequest();
+
+    if (roomState.hasGameBegun)
+        m_screen = Screens::Game;
+
+    if (ImGui::BeginTable("Room data", 2,
+                          ImGuiTableFlags_Resizable |
+                              ImGuiTableFlags_NoSavedSettings |
+                              ImGuiTableFlags_Borders)) {
+        ImGui::TableNextRow();
+        ImGui::TableNextColumn();
+        ImGui::Text("Players");
+        ImGui::TableNextColumn();
+        for (auto i : roomState.players)
+            ImGui::Text(i.c_str());
+
+        ImGui::TableNextRow();
+        ImGui::TableNextColumn();
+        ImGui::Text("Question count");
+        ImGui::TableNextColumn();
+        ImGui::Text(std::to_string(roomState.questionCount).c_str());
+
+        ImGui::TableNextRow();
+        ImGui::TableNextColumn();
+        ImGui::Text("Answer timout");
+        ImGui::TableNextColumn();
+        ImGui::Text(std::to_string(roomState.answerTimeout).c_str());
+
+        ImGui::EndTable();
+    }
+
+    if (m_isAdmin) {
+        if (ImGui::Button("Start game")) {
+            RequestHandler::startGameRequest();
+            m_screen = Screens::Game;
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Close room")) {
+            RequestHandler::closeRoomRequest();
+            m_screen = Screens::Menu;
+        }
+    } else {
+        if (ImGui::Button("Leave room")) {
+            RequestHandler::leaveRoomRequest();
+            m_screen = Screens::Menu;
+        }
+    }
 }
